64afa9d71b27547e306a9ec19c4860fbf8ebfd65
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-11-02 11:06:55 -0300

    crud employees creation

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Employee;
+
+class EmployeeController extends Controller
+{
+	
+    public function listEmployees(Request $request)
+    {
+    	return Employee::get();
+    }
+
+    public function getEmployee(Request $request)
+    {
+    	return Employee::findOrFail($request->id);
+    }
+
+    public function createEmployee(Request $request)
+    {
+    	$Employee = new Employee($request->all());
+    	$Employee->save();
+
+    	return $Employee;
+    }
+
+    public function editEmployee(Request $request)
+    {
+    	$Employee = Employee::findOrFail($request->id);
+    	$Employee->update($request->all());
+
+    	return $Employee;
+    }
+
+    public function deleteEmployee(Request $request)
+    {
+    	Employee::findOrFail($request->id)->delete();
+    }
+
+}
+
